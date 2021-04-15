e9df6f359719ae09f7e0e1fa96ea4544384910d3
Sridevi15k <sridevi15k@gmail.com>
2021-04-15 05:41:53 -0500

    equipment

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
+  equipmentBeingEdited: object = null;
 
+  userEquipment: object[] = [
+    {name: 'Habitat dome', isPresent: true},
+    {name: 'Drones', isPresent: false},
+    {name: 'Food containers', isPresent: false },
+    {name: 'Oxygen tanks', isPresent: true}
+  ];
+ 
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  addEquipment(equipmentName: string, ifPresent: boolean) {
+      this.userEquipment.push({name: equipmentName, isPresent: ifPresent});
+  }
+
+  editEquipment(equipment: object) {
+    this.equipmentBeingEdited = equipment;
+  }
+
+  removeEquipment(equipment: object) {
+    let index = this.userEquipment.indexOf(equipment);
+    this.userEquipment.splice(index, 1);
+
+  }  
+
+  saveEquipment(equipmentName: string, equipment: object) {
+      equipment['name'] = equipmentName;
+      this.equipmentBeingEdited = null;
   }
 
 }
